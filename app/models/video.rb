b20ba5d34056692af97003531208991f649e70db
Andrew Mao <andrewmao@Andrews-MacBook-Pro-3.local>
2021-09-12 05:01:01 -0500

    initial

---
+++ app/models/video.rb
@@ -0,0 +1,8 @@
+class Video < ApplicationRecord
+    has_one_attached :clip
+    has_one_attached :thumbnail
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :clip, presence: true
+    validates :thumbnail, presence: true
+end
